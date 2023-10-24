39d987421ff1a43351434218584afe87a1de3095
adrianlklitzke <93690677+adrianlklitzke@users.noreply.github.com>
2023-10-23 21:36:40 -0300

    Add files via upload

--- database_controller.py
+++ database_controller.py
@@ -1,3 +1,5 @@
+from multiprocessing import get_all_start_methods
+from pkgutil import get_data
 from estufa_model import EstufaModel
 from possue_id_igual import possueIdIgual
 from exception_model import EqualIdExceptionModel
@@ -8,5 +10,14 @@ def criarNovaEstufa(novaEstufa: EstufaModel):
         raise EqualIdExceptionModel
     db.append(novaEstufa)
 
+def get_list(id=None):
+
+  if id is not None:
+    try:
+      return [get_data(id)]
+    except KeyError:
+      raise ValueError("ID não encontrado.")
+  else:
+    return [get_data(id) for id in get_all_start_methods()]
 
 
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from estufa_model import EstufaModel
 from resposta_criar_nova_estufa import RespostaCriarNovaEstufa
 from fastapi.exceptions import HTTPException
 from exception_model import EqualIdExceptionModel, ExceptionModel
-
 app = FastAPI()
 
 @app.post("/criarNovaEstufa/")
@@ -14,3 +13,10 @@ async def criarNovaEstufaDef(novaEstufa: EstufaModel):
         return RespostaCriarNovaEstufa(codigo='criado', mensagem='Sua estufa ' + novaEstufa.nome + ' criada')
     except EqualIdExceptionModel:
         return HTTPException(status_code=400, detail= ExceptionModel(codigo='equalStoveId', menssagem='Id Existente'))
+    
+@app.get("/consultarEstufa/")
+async def get_estufa(id: int):
+    try:
+         return get_estufa(id)
+    except KeyError:
+        raise HTTPException(status_code=404, detail=ExceptionModel(codigo='estufaNaoEncontrada', menssagem='Estufa não encontrada.'))
